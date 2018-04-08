8f54ccf92763794f74400f4bff507f8ded1a13aa
AlanZhangQ <13671240056@163.com>
2018-04-08 23:11:50 +0800

    Alan project

--- sdf/ViewController.m
+++ sdf/ViewController.m
@@ -16,7 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.N
+    NSString * string = @"1==2";
+    
+   NSData *data =[string dataUsingEncoding:NSUTF8StringEncoding];
+    BOOL a = [string boolValue];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"xiancheng=%@",[NSThread currentThread]);
+    });
+    NSLog(@"%@",[NSThread currentThread]);
+    
 }
 
 
